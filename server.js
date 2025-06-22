ad9ff30da9a69367f6d950cfa69406f48557d921
hanbaedal <dawon.today@gmail.com>
2025-06-23 04:15:11 +0900

    오늘의게임

--- server.js
+++ server.js
@@ -47,11 +47,11 @@ async function connectToMongoDB() {
     try {
         console.log('MongoDB 연결 시도:', MONGODB_URI);
         const client = new MongoClient(MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             serverSelectionTimeoutMS: 10000,
             socketTimeoutMS: 45000,
             connectTimeoutMS: 10000,
+            maxPoolSize: 10,
+            minPoolSize: 1,
         });
         await client.connect();
         db = client.db(DB_NAME);
