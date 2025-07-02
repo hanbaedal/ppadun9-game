5d759ac0b9e5b56547cba580472c0910e4f37292
ppadun9-game <dawom.today@gmail.com>
2025-07-02 22:56:05 +0900

    Fix SESSION_SECRET environment variable check

--- server.js
+++ server.js
@@ -840,9 +840,7 @@ async function startServer() {
             if (!process.env.MONGODB_URI) {
                 throw new Error('MONGODB_URI 환경변수가 설정되지 않았습니다.');
             }
-            if (!process.env.SESSION_SECRET) {
-                throw new Error('SESSION_SECRET 환경변수가 설정되지 않았습니다.');
-            }
+            // SESSION_SECRET은 기본값이 있으므로 체크하지 않음
         }
         
         console.log('[Server] MongoDB 연결 시도...');
